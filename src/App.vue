fe7c154fc4e5ac510237a8fe01e5f92efe5b282c
@mhlinskaia <mhlinskaia@dataart.com>
2024-08-02 20:29:56 +0200

    Added interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div>
+    <div>
+      Likes: <strong>{{ likes }}</strong>
     </div>
-    <router-view />
+    <div>
+      Dislikes: <strong>{{ dislikes }}</strong>
+    </div>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+};
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
